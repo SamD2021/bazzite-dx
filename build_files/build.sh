422d41db133f1b0a6e5990fbe1c748f5fd4d9968
Sam Dasilva <samuelramos852@gmail.com>
2025-09-17 19:28:45 -0400

    refactor(CI): Get closer to image-template
    Signed-off-by: Sam Dasilva <samuelramos852@gmail.com>

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,6 +2,25 @@
 
 set -ouex pipefail
 
+### Setup things
+echo -e '[composefs]\nenabled = yes\n\n[root]\ntransient = true' >/usr/lib/ostree/prepare-root.conf && ostree container commit
+
+mkdir -p /nix && ostree container commit
+
+curl -Lo /etc/yum.repos.d/_copr_ryanabx-cosmic.repo "https://copr.fedorainfracloud.org/coprs/ryanabx/cosmic-epoch/repo/fedora-$(rpm -E %fedora)/ryanabx-cosmic-epoch-fedora-$(rpm -E %fedora).repo" &&
+  curl -Lo /etc/yum.repos.d/_copr_sneexy-zen-browser.repo "https://copr.fedorainfracloud.org/coprs/sneexy/zen-browser/repo/fedora-$(rpm -E %fedora)/sneexy-zen-browsder-fedora-$(rpm -E %fedora).repo" &&
+  ostree container commit
+
+rpm --import https://repo.cider.sh/RPM-GPG-KEY &&
+  cat >/etc/yum.repos.d/cider.repo <<'EOF'
+[cidercollective]
+name=Cider Collective Repository
+baseurl=https://repo.cider.sh/rpm/RPMS
+enabled=1
+gpgcheck=1
+gpgkey=https://repo.cider.sh/RPM-GPG-KEY
+EOF
+
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
@@ -10,7 +29,18 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y screen \
+  zsh \
+  cosmic-desktop \
+  kitty \
+  virt-manager \
+  neovim \
+  nmap \
+  blueman \
+  pasystray \
+  network-manager-applet \
+  pcsc-lite \
+  Cider
 
 # Use a COPR Example:
 #
@@ -22,3 +52,4 @@ dnf5 install -y tmux
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+systemctl disable sddm && systemctl enable cosmic-greeter && ostree container commit
